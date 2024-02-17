8330faafe270718ff9bf23fd3cd7e9d4e2303dea
bluesun147 <bluesun147@naver.com>
2024-02-17 23:14:50 +0900

    [Fix] 수익, 계약서 entity 수정
    수익 : ost, 유통사 컬럼 -> 계약서에 이미 fk로 존재
    계약서 : 제작사, 가창자 컬럼 -> ost에 이미 fk로 존재

--- src/main/java/com/haechan/mysettlement/domain/revenue/service/RevenueService.java
+++ src/main/java/com/haechan/mysettlement/domain/revenue/service/RevenueService.java
@@ -1,5 +1,7 @@
 package com.haechan.mysettlement.domain.revenue.service;
 
+import com.haechan.mysettlement.domain.contract.entity.Contract;
+import com.haechan.mysettlement.domain.contract.repository.ContractRepository;
 import com.haechan.mysettlement.global.config.ExcelHelper;
 import com.haechan.mysettlement.domain.distributor.entity.Distributor;
 import com.haechan.mysettlement.domain.distributor.repository.DistributorRepository;
@@ -15,6 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +26,7 @@ public class RevenueService {
     private final RevenueRepository revenueRepository;
     private final OstRepository ostRepository;
     private final DistributorRepository distributorRepository;
+    private final ContractRepository contractRepository;
 
     // 이거에 대한 테스트 만들어야 함.
     public void uploadFile(MultipartFile file) {
@@ -34,9 +38,10 @@ public class RevenueService {
                 Ost ost = ostRepository.findById(dto.getOstId()).orElseThrow();
                 Distributor distributor = distributorRepository.findById(dto.getDistributorId()).orElseThrow();
 
+                Contract contract = contractRepository.findByOstAndDistributor(ost, distributor).orElseThrow();
+
                 Revenue revenue = Revenue.builder()
-                        .ost(ost)
-                        .distributor(distributor)
+                        .contract(contract)
                         .fee(dto.getFee())
                         .date(dto.getDate())
                         .build();
