bea95c2f0e406fe71b0348e6a409a03e9ada4af3
bluesun147 <bluesun147@naver.com>
2024-02-20 23:16:53 +0900

    [Test] batch + scheduler 테스트

--- src/main/java/com/haechan/mysettlement/domain/settlement/repository/SettlementRepository.java
+++ src/main/java/com/haechan/mysettlement/domain/settlement/repository/SettlementRepository.java
@@ -1,8 +1,10 @@
 package com.haechan.mysettlement.domain.settlement.repository;
 
 import com.haechan.mysettlement.domain.settlement.entity.Settlement;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface SettlementRepository extends JpaRepository<Settlement, Long>, SettlementRepositoryCustom {
+    Page<Settlement> findAll(Pageable pageable);
 }
