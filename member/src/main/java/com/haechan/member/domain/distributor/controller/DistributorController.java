8d0340a84f563cb7db330cb6386e5e74b729b0e2
bluesun147 <bluesun147@naver.com>
2024-03-11 01:43:19 +0900

    [Refactor] #21 gateway 설정

--- member/src/main/java/com/haechan/member/domain/distributor/controller/DistributorController.java
+++ member/src/main/java/com/haechan/member/domain/distributor/controller/DistributorController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/distributor")
+@RequestMapping("/member/distributor")
 @RequiredArgsConstructor
 public class DistributorController {
 
