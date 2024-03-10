1c22f110439e0c223d3d2660be9c246c1a037948
bluesun147 <bluesun147@naver.com>
2024-03-11 00:43:28 +0900

    [Refactor] #21 Eureka 설정

--- eureka-server/src/main/java/com/haechan/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/haechan/eurekaserver/EurekaServerApplication.java
@@ -1,10 +1,8 @@
 package com.haechan.eurekaserver;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
