ae832a8521a0714fa4828e70bf592712eeaf82cd
bluesun147 <bluesun147@naver.com>
2024-03-19 00:15:44 +0900

    [Feat] 계약서 등록 중 오류 발생 시 화면에 메시지 출력

--- src/main/java/com/haechan/content/domain/contract/controller/ContractController.java
+++ src/main/java/com/haechan/content/domain/contract/controller/ContractController.java
@@ -3,6 +3,7 @@ package com.haechan.content.domain.contract.controller;
 import com.haechan.content.domain.contract.entity.Contract;
 import com.haechan.content.domain.contract.service.ContractService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -11,10 +12,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/content/contract")
 @RequiredArgsConstructor
@@ -24,7 +27,10 @@ public class ContractController {
     private static final int pageSize = 3;
 
     @GetMapping("/list")
-    public String index(Model model, @PageableDefault(size = pageSize, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
+    public String index(
+            Model model,
+            @PageableDefault(size = pageSize, sort = "id", direction = Sort.Direction.ASC) Pageable pageable,
+            @ModelAttribute("errorMessage") String errorMessage) {
         Page<Contract> contractList = contractService.getContractList(pageable);
         model.addAttribute("contractList", contractList);
 
@@ -33,14 +39,24 @@ public class ContractController {
             pageIndex.add(i);
         }
 
+        log.debug("errorMessage = {}", errorMessage);
+
+        model.addAttribute("errorMessage", errorMessage);
+
         model.addAttribute("pageIndex", pageIndex);
         return "/contract/contractIndex";
     }
 
     @PostMapping("/upload")
-    public String uploadFile(@RequestParam("file") MultipartFile file) {
-        contractService.uploadFile(file);
-
-        return "redirect:/content/contract/list?page=0";
+    public String uploadFile(@RequestParam("file") MultipartFile file, Model model, RedirectAttributes re) {
+        try {
+            contractService.uploadFile(file);
+            re.addFlashAttribute("errorMessage", "");
+            return "redirect:/content/contract/list?page=0";
+        } catch (RuntimeException e) {
+            log.info("e.getMessage() = {}", e.getMessage());
+            re.addFlashAttribute("errorMessage", e.getMessage());
+            return "redirect:/content/contract/list?page=0"+"&errorMessage=" + e.getMessage();
+        }
     }
 }
--- src/main/java/com/haechan/content/global/BaseResponseStatus.java
+++ src/main/java/com/haechan/content/global/BaseResponseStatus.java
@@ -8,21 +8,25 @@ public enum BaseResponseStatus {
     /**
      * 1000: 요청 성공
      */
-    SUCCESS(true, 1000, "요청에 성공하였습니다."),
+    SUCCESS(true, 1000, "Success!"),
 
     /**
      *  2000: Request 오류
      */
-    NULL_TOKEN(false, 2000, "토큰 값을 입력해주세요."),
+    // NULL_TOKEN(false, 2000, "토큰 값을 입력해주세요."),
+    NULL_TOKEN(false, 2000, "Null Token!"),
 
-    INVALID_CONTRACT(false, 3001, "이미 존재하는 계약서 입니다."),
+    // 이미 존재하는 계약서
+    INVALID_CONTRACT(false, 3001, "The Contract Already Exists!"),
 
-    NON_EXIST_OST(false, 3003, "존재하지 않는 ost 입니다."),
+    // 존재하지 않는 ost
+    NON_EXIST_OST(false, 3003, "There's No Such OST!"),
 
     /**
      * 4000: DB, Server 오류
      */
-    DATABASE_ERROR(false, 4000, "데이터베이스 연결에 실패했습니다.");
+    // 데이터베이스 연결에 실패
+    DATABASE_ERROR(false, 4000, "DB connection fail!");
 
     private final boolean isSuccess;
     private final int code;
